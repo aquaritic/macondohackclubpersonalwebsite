106379c82abd4851fea1a0d511511b6c8a370577
aquaritic <krish.s.lad@gmail.com>
2026-04-25 21:35:18 -0500

    Fixed a bunch of naming issues where I named the variables count instead of cookies, also modified grandma to be nebula with a multiplier mechanism and fixed the count not updating (hopefully it works)

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let cookies = 0;
-let Grandma = 0;
-let cookies = localStorage.getItem("cookies");
+let Multiplier = 0;
+let cookies = Number(localStorage.getItem("cookies")) || 0
 
 const cookieDisplay = document.getElementById("cookies");
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const plusFiveBtn = document.getElementById("plusFive");
 const minusFiveBtn = document.getElementById("minusFive");
+const buyNebula = document.getElementById("Nebula")
 const resetBtn = document.getElementById("reset");
 
-buyGrandma.addEventListener("click", () => {
+buyNebula.addEventListener("click", () => {
     if (cookies > 0){
         cookies-=100;
-        Grandma +=1;
+        Multiplier +=1;
         updateColor();
         saveCount();
     }
@@ -20,15 +21,16 @@ buyGrandma.addEventListener("click", () => {
 
 increaseBtn.addEventListener("click", () => {
     cookies++
-    countDisplay.textContent = cookies;
+    cookieDisplay.textContent = cookies;
     updateColor();
+    multiplier();
     saveCount();
 });
 
 decreaseBtn.addEventListener("click", () => {
     if (cookies > 0){
         cookies--;
-        countDisplay.textContent = cookies;
+        cookieDisplay.textContent = cookies;
     }
     updateColor();
     saveCount();
@@ -36,15 +38,16 @@ decreaseBtn.addEventListener("click", () => {
 
 plusFiveBtn.addEventListener("click", () => {
     cookies+=5;
-    countDisplay.textContent = cookies;
+    cookieDisplay.textContent = cookies;
     updateColor();
+    multiplier();
     saveCount();
 });
 
 minusFiveBtn.addEventListener("click", () => {
     if (cookies > 0){
     cookies-=5;
-    countDisplay.textContent = cookies;
+    cookieDisplay.textContent = cookies;
     }
     updateColor();
     saveCount();
@@ -52,39 +55,46 @@ minusFiveBtn.addEventListener("click", () => {
 
 resetBtn.addEventListener("click", () => {
     cookies = 0;
-    countDisplay.textContent = cookies;
+    cookieDisplay.textContent = cookies;
     updateColor();
     saveCount();
 });
 
 function updateColor() {
     if (cookies > 0) {
-        countDisplay.style.color = "green";
+        cookieDisplay.style.color = "green";
     } else if (cookies < 0){
-        countDisplay.style.color = "red";
+        cookieDisplay.style.color = "red";
     } else {
-        countDisplay.style.color = "black";
+        cookieDisplay.style.color = "black";
+    }
+}
+
+function multiplier() {
+    if (Multipler == 1){
+        cookies+=1;
     }
 }
 
 document.addEventListener("keydown"), (event) =>{
-    if (event.key === "ArrowUp)") {
-        count++;
+    if (event.key === "ArrowUp") {
+        cookies++;
+        multiplier();
     } else if (event.key === "ArrownDown") {
-        count--;
+        cookies--;
     }
 
-    countDisplay.textContent = count;
+    cookieDisplay.textContent = cookies;
     updateColor();
     saveCount();
 }
 
-if (count === null) {
-    count = 0;
+if (cookies === null) {
+    cookies = 0;
 } else {
-    count = Number(count);
+    cookies = Number(cookies);
 }
 
 function saveCount() {
-    localStorage.setItem("count", count);
+    localStorage.setItem("cookies", cookies);
 }
