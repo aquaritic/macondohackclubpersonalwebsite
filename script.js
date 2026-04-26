43bbd2fb26a76ad8a36d4c0edadc462ef9f0bc7f
aquaritic <krish.s.lad@gmail.com>
2026-04-26 11:06:45 -0500

    Scrapped the decrease/reset buttons, shifting towards an actual clicker game, hopefully also fixed multiplier so that it works and removed -5 +5 buttons too.

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-let Multiplier = 0;
+let multiplier = 0;
 let cookies = Number(localStorage.getItem("cookies")) || 0
 
 const cookieDisplay = document.getElementById("cookies");
 const increaseBtn = document.getElementById("increase");
-const decreaseBtn = document.getElementById("decrease");
-const plusFiveBtn = document.getElementById("plusFive");
-const minusFiveBtn = document.getElementById("minusFive");
 const buyNebula = document.getElementById("Multiplier")
-const resetBtn = document.getElementById("reset");
 
 buyNebula.addEventListener("click", () => {
-    if (cookies > 0){
+    if (cookies == 100){
         cookies-=100;
-        Multiplier +=1;
+        multiplier +=1;
         updateColor();
         saveCount();
         cookieDisplay.textContent = cookies;
@@ -28,39 +24,6 @@ increaseBtn.addEventListener("click", () => {
     console.log("clicked");
 });
 
-decreaseBtn.addEventListener("click", () => {
-    if (cookies > 0){
-        cookies--;
-        cookieDisplay.textContent = cookies;
-    }
-    updateColor();
-    saveCount();
-});
-
-plusFiveBtn.addEventListener("click", () => {
-    cookies+=5;
-    cookieDisplay.textContent = cookies;
-    updateColor();
-    multiplier();
-    saveCount();
-});
-
-minusFiveBtn.addEventListener("click", () => {
-    if (cookies > 0){
-    cookies-=5;
-    cookieDisplay.textContent = cookies;
-    }
-    updateColor();
-    saveCount();
-});
-
-resetBtn.addEventListener("click", () => {
-    cookies = 0;
-    cookieDisplay.textContent = cookies;
-    updateColor();
-    saveCount();
-});
-
 function updateColor() {
     if (cookies > 0) {
         cookieDisplay.style.color = "green";
@@ -72,22 +35,9 @@ function updateColor() {
 }
 
 function multiplier() {
-    if (Multiplier == 1){
+    if (multiplier == 1){
         cookies+=1;
     }
-}
-
-document.addEventListener("keydown"), (event) => {
-    if (event.key === "ArrowUp") {
-        cookies++;
-        multiplier();
-    } else if (event.key === "ArrowDown") {
-        cookies--;
-    }
-
-    cookieDisplay.textContent = cookies;
-    updateColor();
-    saveCount();
 }
 
 if (cookies === null) {
